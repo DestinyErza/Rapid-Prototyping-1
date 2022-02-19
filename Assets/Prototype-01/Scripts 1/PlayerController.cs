ccee14987325b0c24f730be0f036e745b80148a1
Des <destiny.hession@gmail.com>
2022-02-20 04:38:28 +1100

    03-NewMechanics
    + working wall powerup and instakill
    not working: falling object trap

--- Assets/Prototype-01/Scripts 1/PlayerController.cs
+++ Assets/Prototype-01/Scripts 1/PlayerController.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private SpawnManager SM;
     private GameObject focalPoint;
     public GameObject powerupIndicator;
+    public GameObject Wall;
 
     public float speed = 5.0f;
     public float powerupStrength = 15.0f;
 
     public bool hasPowerup;
+    public bool hasInstaKill;
+
+
 
 
 
     void Start()
     {
+        SM = GetComponent<SpawnManager>();
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
     }
@@ -30,6 +36,11 @@ public class PlayerController : MonoBehaviour
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
         //places poweup below player
         powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+
+        if (hasInstaKill == true)
+        {
+            hasPowerup = false;
+        }
     }
 
 
@@ -46,7 +57,35 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
+
+        //turns on wall+ rns ienumerator to turn it off
+        if (other.CompareTag("WallUP"))
+        {
+            Wall.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(WallCountdownRoutine());
+        }
+
+        if (other.CompareTag("InstaKill"))
+        {
+            hasInstaKill = true;
+            Destroy(other.gameObject);
+            StartCoroutine(IKCountdownRoutine());
+        }
+
+        //trap triggers falling objects
+
+
+        /*if (other.CompareTag("Trap"))
+        {
+            Debug.Log("uh oh");
+            Destroy(other.gameObject);
+            SM.SpawnFall();
+        }*/
     }
+
+
+    
 
     /// <summary>
     /// after powerup is picked up, wait (seconds) until it is turned off + turns off inddicator
@@ -58,7 +97,18 @@ public class PlayerController : MonoBehaviour
         hasPowerup = false;
         powerupIndicator.gameObject.SetActive(false);
     }
-
+    //wallcountdown
+    IEnumerator WallCountdownRoutine()
+    {
+        yield return new WaitForSeconds(10);
+        Wall.gameObject.SetActive(false);
+    }
+    //InstaKillcountdown
+    IEnumerator IKCountdownRoutine()
+    {
+        yield return new WaitForSeconds(10);
+        hasInstaKill = false;
+    }
 
     /// <summary>
     /// when player collides with enemy + has powerup / powerup is applied
@@ -75,5 +125,13 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Collided with" + collision.gameObject.name + "with powerup set to" + hasPowerup);
             enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
         }
+
+        else if (collision.gameObject.CompareTag("Enemy") && (hasInstaKill == true))
+        {
+            Debug.Log("YOOOOOOO");
+            Destroy(collision.gameObject);
+        }
     }
+
+
 }
--- Assets/Prototype-01/Scripts 1/SpawnManager.cs
+++ Assets/Prototype-01/Scripts 1/SpawnManager.cs
@@ -11,6 +11,13 @@ public class SpawnManager : MonoBehaviour
     public int waveNumber;
 
     public GameObject powerupPrefab;
+    public GameObject WallPowerUp;
+    public GameObject InstaKill;
+    public GameObject Trap_Falling;
+
+  
+
+
 
     /// <summary>
     /// spawns enemy at random spawn location using the instantiation of a prefab (+ one per each wave for both enemy and powerups) (waves triggered when enemy count = 0)
@@ -29,8 +36,20 @@ public class SpawnManager : MonoBehaviour
         {
             waveNumber++; SpawnEnemyWave(waveNumber);
             Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+
+
+            Instantiate(WallPowerUp, GenerateSpawnPosition(), WallPowerUp.transform.rotation);
+
+            Instantiate(InstaKill, GenerateSpawnPosition(), InstaKill.transform.rotation);
         }
     }
+
+    /* public void SpawnFall()
+     {
+         Debug.Log("test");
+        Instantiate(Trap_Falling, GenerateSpawnPosition(), Trap_Falling.transform.rotation)
+     }*/
+
     /// <summary>
     /// spawns enemy wave when enemies are cleared
     /// </summary>
