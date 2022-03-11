f92d9c3e14498350ee25da67bce907f0dbe9c106
Des <destiny.hession@gmail.com>
2022-03-12 01:02:37 +1100

    P2-01
    working Dialogue and Scene Set Up!

---
+++ Assets/Prototype-02/Scripts 2/DialogueSystem.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public static DialogueSystem instance;
+    public ELEMENTS elements;
+   
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Start()
+    {
+        isWaitingForInput = false;
+      //  speechPanel.SetActive(false);
+    }
+    public void Update()
+    {
+
+    }
+    
+
+    /// <summary>
+    /// shows speech in dialogue box
+    /// </summary>
+    public void say(string speech, string name = "")
+    {
+        StopSpeaking();
+        speaking = StartCoroutine(Speaking(speech, name ));
+    }
+
+    public void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+    [HideInInspector] public bool isWaitingForInput = false;
+    public bool isSpeaking { get { return speaking != null; } }
+    Coroutine speaking = null;
+    IEnumerator Speaking(string targetSpeech, string name = "")
+    {
+        speechPanel.SetActive(true);
+        speech.text = "";
+        speakerName.text = DetermineSpeaker(name);
+
+        while(speech.text != targetSpeech)
+        {
+            speech.text += targetSpeech[speech.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+        //text finished
+        isWaitingForInput = true;
+        while (isWaitingForInput)
+            yield return new WaitForEndOfFrame();
+        StopSpeaking();
+    }
+
+    string DetermineSpeaker(string s)
+    {
+        string retVal = speakerName.text;
+        if (s != speakerName.text && s != "")
+            retVal = (s.ToLower().Contains("narrator")) ? "" : s;
+               return retVal;
+    }
+
+    /// <summary>
+    /// contains all dilogue related elements from ui
+    /// </summary>
+    [System.Serializable]
+    public class ELEMENTS
+    {
+        public GameObject speechPanel;
+        public TMP_Text speakerName;
+        public TMP_Text speech;
+    }
+    public GameObject speechPanel { get { return elements.speechPanel; } }
+    public TMP_Text speakerName { get { return elements.speakerName; } }
+    public TMP_Text speech { get { return elements.speech; } }
+}
---
+++ Assets/Prototype-02/Scripts 2/Test.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    DialogueSystem dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = DialogueSystem.instance;
+    }
+
+    public string[] s = new string[]
+    {
+        "Test A :Test Name",
+        "test b",
+        "test c",
+        "uh oh: test name bbb"
+    };
+
+    int index = 0;
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!dialogue.isSpeaking || dialogue.isWaitingForInput)
+            {
+                if (index >= s.Length)
+                {
+                    return;
+                }
+                Say(s[index]);
+                index++;
+            }
+        }
+    }
+
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+        dialogue.say(speech, speaker);
+    }
+}
